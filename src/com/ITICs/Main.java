db90c9464160645a97e9804bc813b1de28148441
javier romero <javierromero@javiers-MacBook-Air-6.local>
2020-11-28 04:36:27 -0600

    Agregar el proyecto base de JavaBasics

---
+++ src/com/ITICs/Main.java
@@ -0,0 +1,14 @@
+package com.ITICs;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("src/com/ITICs/Main.java");
+        com.alumnoUno.Main.main();
+        com.alumnoUno.Main.mensaje();
+
+    }
+
+
+}
---
+++ src/com/alumnoUno/Main.java
@@ -0,0 +1,13 @@
+package com.alumnoUno;
+
+public class Main {
+
+    public static void main() {
+        System.out.println("src/com/alumnoUno/Main.java/metodo->main");
+    }
+
+
+    public static void mensaje() {
+        System.out.println("src/com/alumnoUno/Main.java/metodo->mensaje");
+    }
+}
